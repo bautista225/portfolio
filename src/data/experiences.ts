2c2536140e0dc2265d4572a60079ded348dcf90a
Juan Bautista García Traver <juangarciatraver@gmail.com>
2025-02-26 18:20:06 +0100

    Updates experiences and some styles

---
+++ src/data/experiences.ts
@@ -0,0 +1,39 @@
+export const EXPERIENCES = [
+  {
+    period: "Septiembre 2023 - Marzo 2024",
+    title: "AI Engineer Intern",
+    company: "Gradiant",
+    description: `    · Developed a comparison tool for Object Tracking models.<br />
+ · Trained, validated, and optimized Computer Vision models.<br />
+ · Handled image & video annotation pipelines.<br />
+ · Managed CI/CD workflows with Git.`,
+    technologies: ".Net, C#, JavaScript, Bash, SQL, NoSQL, git",
+  },
+  {
+    period: "Noviembre 2019 - Septiembre 2022",
+    title: "Co-founder",
+    company: "Wefinz Solutions SL",
+    description: `Fintech startup where I led the development of a cloud-distributed ERP
+ software for SMEs, deployed in more than 10 financial agencies.<br />
+ As full stack developer, I performed modules based on latest market standards:<br />
+   · Sales add-on with automated billing module.<br />
+    · Digitalization of incoming invoices with automated accounting.<br />
+    · Accounting add-on with financial reports.<br />
+    · Automated filing of tax forms for the AEAT.<br />
+    · Automated treasury management through PSD2 connector.<br />
+    · Integrated Biometric Signature for PDF documents.`,
+    technologies: ".Net, C#, JavaScript, Bash, SQL, NoSQL, git",
+  },
+  {
+    period: "Febrero 2019 - Enero 2022",
+    title: "Full Stack Engineer",
+    company: "Irene Solutions SL",
+    description: `· Desarrollé un complemento de conciliación bancaria automatizada para SAP Business One.<br />
+  · Desarrollé un flujo de trabajo de gestión de documentos con un punto final de SAP.<br />
+  · Implementé una solución de firma biométrica para documentos PDF.<br />
+  · Mantuve una extracción automatizada de documentos con un punto final de digitalización.<br />
+  · Implementé y desplegué Mantis bug tracker en empresa cliente.<br />
+  · Gestioné un conjunto de infraestructura de sistemas de virtualización ESXi.`,
+    technologies: ".Net, C#, JavaScript, Bash, SQL, NoSQL, git",
+  },
+];
